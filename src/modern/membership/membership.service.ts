1eff6e52e8e3f58db4d2b3f7be8ab6d62f25af15
Poorshad <poorshad.Shaddel@redbull.com>
2025-08-12 14:37:35 +0200

    refactor: switch from require to import for UUID in membership service

--- src/modern/membership/membership.service.ts
+++ src/modern/membership/membership.service.ts
@@ -1,4 +1,5 @@
-const { v4: uuidv4 } = require("uuid");
+import { v4 as uuidv4 } from "uuid";
+
 const memberships = require("../../data/memberships.json") as Membership[];
 const membershipPeriods =
 	require("../../data/membership-periods.json") as MembershipPeriod[];
